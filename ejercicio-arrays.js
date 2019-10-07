e4bf70568f3e313c9188b52be3ce0668e7ac713b
inoriega <ivan.noriega@mercadolibre.com>
2019-10-07 16:44:38 -0300

    first commit

---
+++ ejercicio-arrays.js
@@ -0,0 +1,11 @@
+// 1 - Arreglar el header (Header Menu Area)
+
+// 2 - Arreglar el corrusel (Hero Carousel)
+
+// 3 - Arreglar el listado de productos destacados (Trending product)
+
+// 4 - Arreglar el listado de productos mas vendidos (Best Selling item)
+
+// 5 - Arreglar el listado de posts (Blog section)
+
+// 6 - Arreglar el footer (footer)
